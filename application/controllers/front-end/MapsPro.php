3f30726fc1c91b21fad57fe20face066504d4d03
hailv <zefredzocohen@gmail.com>
2016-10-01 09:13:49 +0700

    add curl and test connect server

--- application/controllers/front-end/MapsPro.php
+++ application/controllers/front-end/MapsPro.php
@@ -2,6 +2,9 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+require 'C:\xampp\htdocs\git\1.Codeigniter\1.Map\MapPro\application\third_party\vendor\autoload.php';
+
+use \Curl\Curl;
 
 class MapsPro extends CI_Controller {
 
@@ -10,22 +13,45 @@ class MapsPro extends CI_Controller {
     }
 
     public function index() {
-        $data = array();
-//        $data['deviceInfo'] = $this->db->from('modem_info')->get()->result();
-        $data['devicesInfo'] = array(
-            array('lat'=>21.029692,'lng'=>105.801643,'title'=>'Nga tu Cau Giay','maps_id'=>1),
-            array('lat'=>21.036732,'lng'=>105.779617,'title'=>'Nga tu Xuan Thuy, Pham Hung','maps_id'=>2),
-            array('lat'=>21.030326,'lng'=>105.787996,'title'=>'Nga tu Duy Tan, Tran Thai Tong','maps_id'=>3)
-        );
-        var_dump($this->curl->post('https://128.199.193.255:8000/api/authenticate',array('username'=>'admin','password'=>'admin')));
-        
+//        $data = array();
+////        $data['deviceInfo'] = $this->db->from('modem_info')->get()->result();
+//        $data['devicesInfo'] = array(
+//            array('lat'=>21.029692,'lng'=>105.801643,'title'=>'Nga tu Cau Giay','maps_id'=>1),
+//            array('lat'=>21.036732,'lng'=>105.779617,'title'=>'Nga tu Xuan Thuy, Pham Hung','maps_id'=>2),
+//            array('lat'=>21.030326,'lng'=>105.787996,'title'=>'Nga tu Duy Tan, Tran Thai Tong','maps_id'=>3)
+//        );
+
+
+        $curl = new Curl();
+        $curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+        $curl->setOpt(CURLOPT_SSL_VERIFYHOST, FALSE);
+        $curl->setOpt(CURLOPT_CONNECTTIMEOUT, 0);
+        $curl->setOpt(CURLOPT_VERBOSE, 1);
+        $curl->setOpt(CURLOPT_TIMEOUT, 4000);
+        $curl->post('https://128.199.193.255:8000/api/authenticate', array(
+            'username' => 'admin',
+            'password' => 'admin'
+        ));
+        if ($curl->error) {
+            echo 1;
+            echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage;
+        } else {
+            echo 2;
+            echo 'Response:' . "\n";
+            var_dump($curl->response);
+        }
+
+//        $curl->setHeader('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiYWRkcmVzcyI6Ijo6ZmZmZjoxMTMuMjMuMTI3LjI1IiwiaWF0IjoxNDc1Mjg1MTYyLCJleHAiOjE0NzUzNzE1NjJ9.IcQNTikpmLTnrhe6XfJoN4dL-CETWkrCMk29WbHe4O8');
+//        $list = $curl->get('https://128.199.193.255:8000/api/device-list');
+//        var_dump($list);
         die;
         $data['temp'] = 'front-end/maps/index';
 //        $data['debug'] = 'front-end/block/view_maker';
-        $this->load->view('front-end/template/master',$data);
+        $this->load->view('front-end/template/master', $data);
     }
-    public function search(){
-        echo json_encode(array(1=>1));
+
+    public function search() {
+        echo json_encode(array(1 => 1));
         exit;
     }
 
--- application/libraries/Curl.php
+++ application/libraries/Curl.php
@@ -9,7 +9,7 @@ class Curl {
     var $proxy;
 
     function Curl($cookies = TRUE, $cookie = 'cookies.txt', $compression = 'gzip', $proxy = '') {
-        $this->headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';
+        $this->headers[] = 'application/json';
         $this->headers[] = 'Connection: Keep-Alive';
         $this->headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
         $this->user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)';
@@ -59,8 +59,8 @@ class Curl {
         curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($process, CURLOPT_HEADER, 1);
 //        curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
-        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
+//        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
+//        curl_setopt($process, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($process, CURLOPT_POST, 1);
         curl_setopt($process, CURLOPT_PORT, 8000);
         curl_setopt($process, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
@@ -75,6 +75,9 @@ class Curl {
         curl_setopt($process, CURLOPT_POSTFIELDS, $data);
         curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($process, CURLOPT_CAINFO, getcwd() . "/public/cacert.pem");
 //        curl_setopt($process, CURLOPT_POST, sizeof($data));
         $return = curl_exec($process);
         curl_close($process);
