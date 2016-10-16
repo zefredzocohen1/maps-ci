204471d880bcbe944b0bf5cd7fd6322742fc51df
hailv <zefredzocohen@gmail.com>
2016-10-16 19:44:01 +0700

    fix add

--- application/config/config.php
+++ application/config/config.php
@@ -44,7 +44,7 @@ $config['base_url'] = $base_url;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
