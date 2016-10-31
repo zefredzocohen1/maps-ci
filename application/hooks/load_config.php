99107b1df3cbe2121854177c1df10df7fd21b7e9
hailv <zefredzocohen@gmail.com>
2016-11-01 06:13:12 +0700

    fix

--- application/hooks/load_config.php
+++ application/hooks/load_config.php
@@ -1,14 +1,14 @@
 <?php
-require APPPATH . 'third_party'.DIRECTORY_SEPARATOR.'php-cache'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-use phpFastCache\CacheManager;
+//require APPPATH . 'third_party'.DIRECTORY_SEPARATOR.'php-cache'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+//use phpFastCache\CacheManager;
 
 
 function load_cache(){
-    $CI = &get_instance();
-    CacheManager::setDefaultConfig(array(
-        "path" => APPPATH.'logs',
-    ));
-    $fileCache = CacheManager::getInstance('files');
-//    $fToken = $fileCache->
-    $CI->config->set_item('fileCache',$fileCache);
+//    $CI = &get_instance();
+//    CacheManager::setDefaultConfig(array(
+//        "path" => APPPATH.'logs',
+//    ));
+//    $fileCache = CacheManager::getInstance('files');
+////    $fToken = $fileCache->
+//    $CI->config->set_item('fileCache',$fileCache);
 }
